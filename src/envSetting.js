4d6e55924ee0e10456e27b561cd7a07e8f9e07b3
John Paul Barrameda <john.barrameda@quantrics.com>
2021-11-17 04:40:38 +0800

    final na kaya

--- src/envSetting.js
+++ src/envSetting.js
@@ -1,3 +1,6 @@
 export const ROUTER_BASE = process.env.REACT_APP_ROUTER_BASE
 
-export const BASE_URL_API  = "https://localhost:44315/";
+//export const BASE_URL_API  = "https://localhost:44315/";
+//heroku link
+
+export const BASE_URL_API  = "https://additionhillsapi.herokuapp.com/";
